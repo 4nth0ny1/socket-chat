0b9d1f980dc66132bc9d3242c9fba326c5360f01
4nth0ny1 <acatullo4@gmail.com>
2022-02-18 18:07:33 -0600

    added new connections to be added/disconnected

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ const messageInput = document.getElementById('message-input')
 const name = prompt('What is your name?')
 appendMessage('You joined')
 socket.emit('new-user', name)
+
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`)
+  })
+  
+  socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+  })
+  
+  socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
+  })
