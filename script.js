a426a41513e93112d3cee5895bb83098ecd2c7a5
4nth0ny1 <acatullo4@gmail.com>
2022-02-18 18:08:01 -0600

    added messaging on frontend

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ socket.on('chat-message', data => {
   socket.on('user-disconnected', name => {
     appendMessage(`${name} disconnected`)
   })
+
+  messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', message)
+    messageInput.value = ''
+  })
+  
+  function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+  }
